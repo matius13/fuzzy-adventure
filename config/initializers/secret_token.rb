b9c26dc28bc35613c4d43c53b08193b688508697
Mateusz Portka <ruda_zosia@o2>
2013-01-27 09:52:55 +0100

    start ksiegarnia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ksiegarnia::Application.config.secret_token = '7f0577ae2a148c7888d51f34299b634f18c631c8da7b3d6b852b798ea09c71b671d3f1e5757b4c71f0b08380995af32e03e5855b1e412f6d0de5b88fa1750d44'
